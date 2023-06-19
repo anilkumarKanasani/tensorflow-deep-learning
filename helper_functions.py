e254b1df19d11ac82756840cd9575de043475ae9
anilkumarKanasani <49880954+anilkumarKanasani@users.noreply.github.com>
2023-06-19 10:49:45 +0200

    Helper_functions added

---
+++ helper_functions.py
@@ -0,0 +1,11 @@
+
+def walk_through_data(raw_data_path:str):
+    import os
+    from glob import glob
+    for fold in os.listdir(raw_data_path):
+        print(fold , "details : ")
+        print("Number of classes : ", len(glob(raw_data_path+"/"+fold+"/*")))
+        for cls in os.listdir(raw_data_path + "/" + fold):
+            print("Class" , cls , "has : ", len(glob(raw_data_path+"/"+fold+"/"+cls+"/*")) , "files")
+        print("-"*35)
+    return None
